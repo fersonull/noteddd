feaf089c3351c728939e2d70f466c9128d925332
fersonull <culaspero@gmail.com>
2025-12-31 14:19:56 +0800

    add dashboard page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,13 @@
 import SignInForm from "@/components/auth/sign-in-form";
 import { auth } from "../auth";
+import DashboardPage from "../dashboard/page";
+import { redirect } from "next/navigation";
 
 export default async function AuthPage() {
   const session = await auth();
   console.log(session);
+
+  if (session) redirect("/dashboard");
 
   return <SignInForm />;
 }
