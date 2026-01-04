9ca1613ce09d2887e4a151d533cc5cbc947d634d
fersonull <culaspero@gmail.com>
2026-01-04 20:25:42 +0800

    add delete notebook action

--- components/notebooks/notebooks-table.tsx
+++ components/notebooks/notebooks-table.tsx
@@ -9,9 +9,17 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { NotebookIcon } from "lucide-react";
+import { Ellipsis, NotebookIcon, Pencil, Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Notebook } from "@/lib/generated/prisma/client";
+import { deleteNotebook } from "@/lib/actions/notebook";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "../ui/dropdown-menu";
 
 type NotebookType = {
   notebooks: Notebook[];
@@ -19,6 +27,14 @@ type NotebookType = {
 
 export function NotebooksTable({ notebooks }: NotebookType) {
   const router = useRouter();
+
+  const handleDelete = async (notebookId: string) => {
+    try {
+      await deleteNotebook(notebookId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Table>
@@ -34,17 +50,47 @@ export function NotebooksTable({ notebooks }: NotebookType) {
         {notebooks.map((n) => (
           <TableRow
             key={n.id}
-            className="cursor-pointer"
+            className="cursor-pointer hover:bg-muted/50 transition-colors"
             onClick={() => router.push(`/notebooks/${n.id}`)}
           >
             <TableCell className="font-medium flex items-center gap-2">
               <NotebookIcon size={16} className="text-muted-foreground" />
               {n.title}
             </TableCell>
-            <TableCell className="text-right">
-              {n.updatedAt.toLocaleString("en-us")}
+            <TableCell className="text-right text-muted-foreground">
+              {n.updatedAt.toLocaleDateString("en-US")}
             </TableCell>
-            <TableCell className="text-right">12.2 KB</TableCell>
+            <TableCell className="text-right text-muted-foreground">
+              12.2 KB
+            </TableCell>
+
+            <TableCell className="text-right w-12.5 p-0 pr-4">
+              <div onClick={(e) => e.stopPropagation()}>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon" className="h-8 w-8">
+                      <Ellipsis className="h-4 w-4" />
+                      <span className="sr-only">Open menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+
+                  {/* Align 'end' prevents the menu from getting cut off */}
+                  <DropdownMenuContent align="end" className="font-outfit">
+                    <DropdownMenuItem onClick={() => console.log("Rename")}>
+                      <Pencil className="mr-2 h-4 w-4" />
+                      Rename
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      className="text-red-600 focus:text-red-600 focus:bg-red-50"
+                      onClick={() => handleDelete(n.id)}
+                    >
+                      <Trash2 className="mr-2 h-4 w-4" />
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
